6240dc5225bbff732e8ef9d41650f8871cce1d30
hiko-ogeqq <knhk0419@gmail.com>
2020-12-19 22:51:40 +0900

    4 end

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,8 +1,28 @@
 package com.example.demo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="user")
 public class User {
-	private int id;
-	private String name;
-	private String address;
-	private String tel;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column
+  private int id;
+  @Column
+  private String name;
+  @Column
+  private String address;
+  @Column
+  private String tel;
 }
